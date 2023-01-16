77691a2703cac9a9b5690fd699ab57027ee7be3b
otterlee9043 <otterlee99@gmail.com>
2023-01-16 23:23:24 +0900

    Feat: codee 파일 생성하는 인터페이스 추가
    디렉토리 트리에서 참조 파일과 새로 생길 codee 파일의  경로를 선택해서 codee 파일을 생성하는 인터페이스 추가
    fetch api로 백엔드에 codee 파일 생성을 요청하는 코드 추가

--- app/static/script/createcodee.js
+++ app/static/script/createcodee.js
@@ -1,37 +1,23 @@
-function browse_dir(event, tree, username, parent_node, location = null) {
-  event.preventDefault();
-  show_dir(tree, username, parent_node, location);
-  console.log("browser");
-}
-
-function open_dir(id) {
-  document.getElementById(id).style.height = "250px";
-  const dir = document.querySelector("#browse1 .focus");
-  console.log(dir);
-}
-
 async function createCodee() {
   const codee_name = document.getElementById("codee_name").value;
-  const codee_path = document.getElementById("browse1_path").value;
-  const ref_path = document.getElementById("browse2_path").value;
+  const codee_path = document.getElementById("codee_path").value;
+  const ref_path = document.getElementById("ref_path").value;
   console.log(codee_path);
 
-  const url = new URL(`${window.origin}/codination/ver1/create_codee`) ;
   const opts = {
     method: "POST",
     body: JSON.stringify({
+      "repo": repo,
       "codee_name": codee_name,
       "codee_path": codee_path,
       "ref_path": ref_path,
-      "username": document.querySelector("li.dir").getAttribute("id")
     }),
     headers: new Headers({
       "content-type": "application/json",
     }),
   };
 
-  // await fetch(`${window.origin}/codination/ver1/create_codee`, opts);
-  await fetch(url, opts)
+  await fetch("/create_codee", opts)
   .then(function (response) {
     if (response.status != 200) {
       console.log(`Looks like there was a problem. Status code: ${response.status}`);
@@ -41,6 +27,6 @@ async function createCodee() {
   .catch(function (error) {
     console.log("Fetch error: " + error);
   });
+  console.log("creating codee file finished!");
   
-  location.href = `/codination/ver1/showfile/${codee_path}/${codee_name}.cd?user=${username}`;
 }
