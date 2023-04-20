d2e00b832fbe2732cf8c254eb9292b1829b8c79a
otterlee9043 <otterlee99@gmail.com>
2023-04-20 23:44:50 +0900

    Refactor: 사용하지 않는 코드 제거

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,8 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_pagedown import PageDown
+from flask_session import Session
 from config import config
-from flask_session import Session, SqlAlchemySessionInterface
 import mysql.connector
 
 
@@ -17,12 +17,13 @@ db = SQLAlchemy()
 pagedown = PageDown()
 
 login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
+login_manager.login_view = "auth.login"
 sess = Session()
+
 
 def create_app(config_name):
     app = Flask(__name__)
-    
+
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
@@ -30,26 +31,24 @@ def create_app(config_name):
     bootstrap.init_app(app)
     moment.init_app(app)
     db.init_app(app)
-    
+
     login_manager.session_protection = None
     login_manager.init_app(app)
     pagedown.init_app(app)
     sess.init_app(app)
 
-    app.config['SESSION_SQLALCHEMY'] = db
-    
+    app.config["SESSION_SQLALCHEMY"] = db
+
     with app.app_context():
-        print("db.create_all()")
         db.create_all()
         db.session.commit()
-    
-    from .api import api as api_blueprint
-    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
 
     from .main import main as main_blueprint
+
     app.register_blueprint(main_blueprint)
 
     from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+
+    app.register_blueprint(auth_blueprint, url_prefix="/auth")
 
     return app
