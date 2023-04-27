a530b5a63bb72beb6e3cceba13903dd60d6e86e3
otterlee9043 <otterlee99@gmail.com>
2023-04-27 22:26:29 +0900

    Refactor: sessions 테이블이 생기지 않는 문제 해결

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,14 @@
 from flask import Flask
-from flask_mail import Mail
 from flask_bootstrap import Bootstrap
-from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_pagedown import PageDown
 from flask_session import Session
 from config import config
 import mysql.connector
 
 
-mail = Mail()
 bootstrap = Bootstrap()
-moment = Moment()
 db = SQLAlchemy()
-pagedown = PageDown()
 
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
@@ -27,19 +21,17 @@ def create_app(config_name):
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    mail.init_app(app)
     bootstrap.init_app(app)
-    moment.init_app(app)
     db.init_app(app)
 
     login_manager.session_protection = None
     login_manager.init_app(app)
-    pagedown.init_app(app)
     sess.init_app(app)
 
     app.config["SESSION_SQLALCHEMY"] = db
 
     with app.app_context():
+        app.session_interface.db.create_all()
         db.create_all()
         db.session.commit()
 
